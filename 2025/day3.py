ed39da963658a223b698ac5c6c472ebfb5ec270e
Connor Basile <cbasile2@gmail.com>
2025-12-03 16:02:57 -0600

    clean up a little

--- 2025/day3.py
+++ 2025/day3.py
@@ -1,23 +1,22 @@
 banks = [list(map(int, line)) for line in open(0).read().split()]
 
-
+# Cleaned up after submission
 def max_joltage(n):
     j = 0
     for bank in banks:
         start = 0
         nums = []
         for end in range(len(bank) - n + 1, len(bank) + 1):
-            m = bank[start]
+            m = 0
             m_i = start
             for i in range(start, end):
                 if bank[i] == 9:
-                    m = 9
                     m_i = i
                     break
                 if bank[i] > m:
                     m = bank[i]
                     m_i = i
-            nums.append(str(m))
+            nums.append(str(bank[m_i]))
             start = m_i + 1
         j += int("".join(nums))
     return j
