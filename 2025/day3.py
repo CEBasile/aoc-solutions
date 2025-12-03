3ce327cddda7342d94fc019f403000920be392b1
Connor Basile <cbasile2@gmail.com>
2025-12-03 11:04:00 -0600

    day 3 completed last night but cleaned up for sharing.

---
+++ 2025/day3.py
@@ -0,0 +1,26 @@
+banks = [list(map(int, line)) for line in open(0).read().split()]
+
+
+def max_joltage(n):
+    j = 0
+    for bank in banks:
+        start = 0
+        nums = []
+        for c in range(len(bank) - n + 1, len(bank) + 1):
+            m = bank[start]
+            m_i = start
+            for i in range(start, c):
+                if bank[i] == 9:
+                    m = 9
+                    m_i = i
+                    break
+                if bank[i] > m:
+                    m = bank[i]
+                    m_i = i
+            nums.append(str(m))
+            start = m_i + 1
+        j += int("".join(nums))
+    return j
+
+
+print(max_joltage(2), max_joltage(12))
