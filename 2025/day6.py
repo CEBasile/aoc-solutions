3ea245c8bb894ceefb77ae5cd6df2734bed1b7ea
Connor Basile <cbasile2@gmail.com>
2025-12-06 02:41:21 -0600

    cleaned up, see prev commit for original

--- 2025/day6.py
+++ 2025/day6.py
@@ -1,4 +1,4 @@
-rows = open(0).readlines()
+rows = open(0).read().splitlines()
 
 t = [line.split() for line in rows]
 problems = [list(row) for row in zip(*t, strict=True)]
@@ -21,25 +21,25 @@ current = []
 total = 0
 
 for i in range(width):
-    col = "".join(row[i] for row in rows)
-    if not col.strip():
-        if current:
-            problems.append(current)
-            current = []
+    col = "".join(row[i] for row in rows).strip()
+    if not col:
+        problems.append(current)
+        current = []
     elif not current:
-        current = [col[-1], col[:-1].strip()]
+        current = [col[-1], col[:-1]]
     else:
-        current.append(col[:-1].strip())
+        current.append(col)
+
+problems.append(current)
 
 for problem in problems:
+    nums = [int(num) for num in problem[1:]]
     match problem[0]:
         case "*":
             c = 1
-            nums = [int(num) for num in problem[1:]]
             for num in nums:
                 c *= num
             total += c
         case "+":
-            nums = [int(num) for num in problem[1:]]
             total += sum(nums)
 print(total)
