1cec69d3a3c196556bfd4388fa3f86a39ee1f78a
Connor Basile <cbasile2@gmail.com>
2025-12-06 11:17:37 -0600

    final version for sharing

--- 2025/day6.py
+++ 2025/day6.py
@@ -1,25 +1,19 @@
+from math import prod
+
 rows = open(0).read().splitlines()
-
 t = [line.split() for line in rows]
+
 problems = [list(row) for row in zip(*t, strict=True)]
+ops = {"*": prod, "+": sum}
+a = b = 0
 
-total = 0
 for problem in problems:
-    if problem[-1] == "*":
-        solution = 1
-        for i in map(int, problem[:-1]):
-            solution *= i
-    else:
-        solution = sum(map(int, problem[:-1]))
-
-    total += solution
-print(total)
+    nums, op = map(int, problem[:-1]), problem[-1]
+    a += ops[op](nums)
 
 width = len(rows[0])
 problems = []
 current = []
-total = 0
-
 for i in range(width):
     col = "".join(row[i] for row in rows).strip()
     if not col:
@@ -33,13 +27,6 @@ for i in range(width):
 problems.append(current)
 
 for problem in problems:
-    nums = [int(num) for num in problem[1:]]
-    match problem[0]:
-        case "*":
-            c = 1
-            for num in nums:
-                c *= num
-            total += c
-        case "+":
-            total += sum(nums)
-print(total)
+    nums, op = map(int, problem[1:]), problem[0]
+    b += ops[op](nums)
+print(a, b)
